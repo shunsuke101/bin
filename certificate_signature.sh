398ae0fe31998c02d7973a1d00ed5fbcc9fe4fb0
shunsuke101 <sasakishunsuke1010@gmail.com>
2026-06-24 09:18:47 +0900

    extend encode

--- certificate_signature.sh
+++ certificate_signature.sh
@@ -1,7 +1,9 @@
 TPM_DIR=$HOME/test/tpm
 SSH_DIR=$HOME/.ssh
 
-
+base64 -w0 $TPM_DIR/quote.msg > $TPM_DIR/quote_msg.b64
+base64 -w0 $TPM_DIR/quote.sig > $TPM_DIR/quote_sig.b64
+base64 -w0 $TPM_DIR/nonce.bin > $TPM_DIR/nonce_bin.b64
 ssh-keygen -s $SSH_DIR/ca.key -I certificate_test -n ubuntu -z 1 \
 	-O extension:quote_msg.b64="$(cat $TPM_DIR/quote_msg.b64)" \
 	-O extension:quote_sig.b64="$(cat $TPM_DIR/quote_sig.b64)" \
